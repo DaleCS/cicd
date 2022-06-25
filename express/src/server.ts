e0d98ba49a3ac349442fe69bd30a705b35b202a9
DaleCS <Dale.Seen@gmail.com>
2022-06-24 22:36:48 -0700

    chore: initial commit

---
+++ express/src/server.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const app: Express = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", async (req: Request, res: Response) => {
+    return res.status(200).json({ msg: "Hello world " });
+});
+
+app.listen(PORT, (): void => {
+    console.log("express server is listening on port: " + PORT);
+});
