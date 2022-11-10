5bc13af2ccf37c3e1078aaefda2e834ea3624de2
DaleCS <Dale.Seen@gmail.com>
2022-11-09 21:35:35 -0800

    script

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Storage } = require("@google-cloud/storage");
+
+const BUCKET_NAME = "cheap_bucket";
+const FILE_NAME = "file.json";
+
+const storageClient = new Storage();
+
+async function downloadFile() {
+    const file = await storageClient.bucket(BUCKET_NAME).file(FILE_NAME).download();
+    const parsedObject = JSON.parse(file[0]);
+    console.log(parsedObject);
+}
+
+downloadFile().then(() => {
+    console.log("DONE!");
+});
